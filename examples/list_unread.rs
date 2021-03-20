36a9c5580a9c8e193bc5d23a52a2d3ed2165b3ef
david-perez <d@vidp.dev>
2021-03-20 22:09:21 +0100

    Add list_unread example program

---
+++ examples/list_unread.rs
@@ -0,0 +1,36 @@
+use pickpocket::{Client, GetInputBuilder, ItemOrDeletedItem, State};
+
+fn client() -> Client {
+    let consumer_key = std::env::var("POCKET_CONSUMER_KEY").expect("POCKET_CONSUMER_KEY not set");
+    let authorization_code =
+        std::env::var("POCKET_AUTHORIZATION_CODE").expect("POCKET_AUTHORIZATION_CODE not set");
+
+    Client {
+        authorization_code,
+        consumer_key,
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let reading_list = client()
+        .get(
+            &GetInputBuilder::default()
+                .state(Some(State::Unread))
+                .build()
+                .unwrap(),
+        )
+        .await
+        .unwrap();
+
+    for item_or_deleted_item in reading_list.values() {
+        match item_or_deleted_item {
+            ItemOrDeletedItem::Item(item) => {
+                println!("{} --- {}", item.resolved_title, item.resolved_url);
+            }
+            ItemOrDeletedItem::DeletedItem(deleted_item) => {
+                println!("Item {} was deleted", deleted_item.item_id);
+            }
+        }
+    }
+}
